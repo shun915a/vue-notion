62deded986114d4aab106bd6e580708bcf828af3
shun915a <shun915a@gmail.com>
2022-10-09 15:51:15 +0900

    Add bootstrap

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <NavigationBar />
+    <MainPage />
+  </div>
+</template>
+
+<script>
+import NavigationBar from '@/components/NavigationBar.vue'
+import MainPage from '@/components/MainPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavigationBar,
+    MainPage
+  }
+}
+</script>
+
+<style>
+#app {
+  height: 100vh;
+}
+</style>
+
